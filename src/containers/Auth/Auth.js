cc40efca5ec69d59732a97b66f3cad740f6cc5dc
Damilola Adekoya <damilolaadekoya@Damilolas-MacBook-Pro.local>
2019-06-18 17:45:42 +0100

    feature(authentication):adding authentication Adding authenticationa and protecting routes

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+
 import classes from './Auth.css';
 import Input from '../../components/UI/Input/Input';
+import Spinner from '../../components/UI/Spinner/Spinner';
 import Button from '../../components/UI/Button/Button';
+import * as actions from '../../store/actions/authAction';
 
 class Auth extends Component {
   state = {
@@ -34,8 +39,66 @@ class Auth extends Component {
         valid: false,
         touched: false
       }
-    } 
+    },
+    isSignup: true,
   }
+
+  checkValidity(value, rules) {
+      let isValid = true;
+      if (!rules) {
+          return true;
+      }
+      
+      if (rules.required) {
+          isValid = value.trim() !== '' && isValid;
+      }
+
+      if (rules.minLength) {
+          isValid = value.length >= rules.minLength && isValid
+      }
+
+      if (rules.maxLength) {
+          isValid = value.length <= rules.maxLength && isValid
+      }
+
+      if (rules.isEmail) {
+          const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+          isValid = pattern.test(value) && isValid
+      }
+
+      if (rules.isNumeric) {
+          const pattern = /^\d+$/;
+          isValid = pattern.test(value) && isValid
+      }
+
+      return isValid;
+  }
+
+inputChangedHandler = (event, controlName) => {
+    const updatedControls = {
+        ...this.state.controls,
+        [controlName]: {
+            ...this.state.controls[controlName],
+            value: event.target.value,
+            valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
+            touched: true
+        }
+    };
+    this.setState({controls: updatedControls});
+}
+
+submitHandler = (event) => {
+    event.preventDefault();
+    this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value, this.state.isSignup);
+}
+
+switchAuthModeHandler = () => {
+  this.setState(prevState => {
+    return {isSignup: !prevState.isSignup}
+  })
+}
+
+
   render () {
     const formElementsArray = [];
     for ( let key in this.state.controls ) {
@@ -45,7 +108,7 @@ class Auth extends Component {
         } );
     }
 
-    const form = formElementsArray.map( formElement => (
+    let form = formElementsArray.map( formElement => (
         <Input
             key={formElement.id}
             elementType={formElement.config.elementType}
@@ -57,15 +120,52 @@ class Auth extends Component {
             changed={( event ) => this.inputChangedHandler( event, formElement.id )} />
     ) );
 
+    if (this.props.loading) {
+      form = <Spinner />
+    }
+
+    let errorMessage = null;
+
+    if (this.props.error) {
+      errorMessage = (
+        <p>{this.props.error.message}</p>
+      )
+    }
+
+    let authRedirect = null;
+    if (this.props.isAuthenticated) {
+      authRedirect = <Redirect to="/" />
+    }
     return (
         <div className={classes.Auth}>
+          {authRedirect}
+          {errorMessage}
             <form onSubmit={this.submitHandler}>
                 {form}
                 <Button btnType="Success">SUBMIT</Button>
             </form>
+            <Button
+              clicked={this.switchAuthModeHandler} 
+              btnType="Danger">SWITCH TO {this.state.isSignup ? 'SIGNIN' : 'SIGNUP'}</Button>
         </div>
     );
-}
+  }
 }
 
-export default Auth;
+const mapStateToprops = state => {
+  return {
+    loading: state.auth.loading,
+    error: state.auth.error,
+    isAuthenticated: state.auth.token !== null
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup))
+  }
+}
+
+
+
+export default connect(mapStateToprops, mapDispatchToProps)(Auth);
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -38,7 +38,11 @@ class BurgerBuilder extends Component {
   }
 
   purchaseHandler = () => {
-    this.setState({purchasing: true});
+    if (this.props.isAuthenticated) {
+      this.setState({purchasing: true});      
+    } else {
+      this.props.history.push('/auth');
+    }
   }
 
   purchaseCancelHandler = () => {
@@ -70,6 +74,7 @@ class BurgerBuilder extends Component {
             price={this.props.price}
             purchasable={this.updatePurchaseState(this.props.ings)}
             ordered={this.purchaseHandler}
+            isAuth={this.props.isAuthenticated}
             disabled={disabledInfo}
           />          
         </Aux>
@@ -101,7 +106,8 @@ const mapStateToProps = (state) => {
   return {
     ings: state.burgerBuilder.ingredients,
     price: state.burgerBuilder.totalPrice,
-    error: state.burgerBuilder.error
+    error: state.burgerBuilder.error,
+    isAuthenticated: state.auth.token !== null,
   }
 }
 
